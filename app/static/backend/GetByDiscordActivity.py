4bfe46ba3a2d0e48087699c426ac434e99b03590
RitsukiShuto <rtk410s@gmail.com>
2023-06-10 14:30:19 +0900

    アクティビティ取得機能を実装

--- app/static/backend/GetByDiscordActivity.py
+++ app/static/backend/GetByDiscordActivity.py
@@ -4,13 +4,77 @@
 import discord
 from discord.ext import commands
 
-import Status       # ステータスクラス
+global game
+global client
 
+class MyClient(discord.Client):
+    # 起動時に動作する処理
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+        # アクティビティを取得するユーザーを指定
+        member = discord.utils.get(self.get_all_members(), name="DoubchBuger")
+
+        # memberがNoneだったらエラーを返す
+        if member == None:
+            print("member is None.")
+            return
+
+        getDiscordActivity(member)
+
+    # メッセージ受信時に動作する処理
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+        # メッセージ送信者がBotだった場合は無視する
+        if message.author.bot:
+            return
+        
+        # メッセージの内容によって返信する
+        if message.content == '/getact':
+            member = discord.utils.get(self.get_all_members())
+            getDiscordActivity(member)
+
+    # プロフィールが更新されたときに動作する処理
+    async def on_member_update(self, before, after):
+        getDiscordActivity(after)
+
+    # webサイトからPOSTされたときに動作する処理
+    # async def on_post(self, request):
+    #     print(request)
+    #     getDiscordActivity();
+
+
+def getDiscordActivity(member):
+    print(member)
+
+    if member.activity == None:
+        game = ""
+        print("None")
+
+    else:
+        game = member.activity.name
+        print(game)
 
 def main():
-    # Botのトークンをテキストファイルから読み込む
-    with open('./token/discoToken.txt', 'r') as f:
-        TOKEN = f.read()
+    # Botのトークンを読み込む
+    try:                            # webAppから起動した時
+        with open('token/discoToken.txt', 'r') as f:
+            TOKEN = f.read()
+    
+    except FileNotFoundError:       # デバッガから起動した時
+        print("Token file is not found.")
+        print("open token file by debug mode.")
+
+        with open('app/static/backend/token/discoToken.txt', 'r') as f:
+            TOKEN = f.read()
+
+    # Intentsオブジェクトを生成
+    intents = discord.Intents.all()
+
+    # クライアントのインスタンスを生成
+    client = MyClient(intents=intents)
+    client.run(TOKEN)
 
 if __name__ == '__main__':
     main()
